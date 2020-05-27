039ee3409052800ff3b721ab0b0c4f6c959aec4e
Deepak Kamboj <deepakredmi111001@gmail.com>
2020-05-27 08:59:00 +0530

    Reactive Forms 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import 'hammerjs';
@@ -62,7 +65,10 @@ import { LeaderService} from './services/leader.service';
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
     
   ],
   providers: [DishService,
