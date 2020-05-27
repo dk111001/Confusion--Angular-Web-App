80c52bd296a2aacf086553f4257cda1613093400
Deepak Kamboj <deepakredmi111001@gmail.com>
2020-05-27 09:26:24 +0530

    Reactive Forms 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 
@@ -13,7 +13,10 @@ export class ContactComponent implements OnInit {
 
   feedbackForm : FormGroup;
   feedback: Feedback;
-  conatctType = ContactType;
+  contactType = ContactType;
+
+  @ViewChild('fform') feedbackFormDirective;
+  
 
   constructor(private fb: FormBuilder) {
       this.createForm();
@@ -23,20 +26,33 @@ export class ContactComponent implements OnInit {
   }
   createForm(){
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: ['', Validators.required ],
+      email: ['', Validators.required ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
   }
-
+ 
   onSubmit() {
+
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedbackFormDirective.resetForm();
   }
+
+  
+  
 
 }
